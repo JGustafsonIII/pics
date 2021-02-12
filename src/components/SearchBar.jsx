46dfb09aaac1b4d81ca3ced0421698778805fc7c
John Gustafson <John.Gustafson1@Marist.edu>
2021-02-12 13:54:57 -0500

    Set up axios request to the Unsplash API

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,10 +3,18 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
+  // Using arrow methods automatically binds the variables so we don't end up with an undefined state
+  // A similar solution would be to make onSubmit={() => this.onFormSubmit()} since this would bind the state there instead
+  // allowing us to use regular functions rather then arrow methods
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
       <div className='ui segment'>
-        <form className='ui form'>
+        <form className='ui form' onSubmit={this.onFormSubmit}>
           <div className='field'>
             <label>Image Search</label>
             <input
